5a7944f1ed0bd5507c92ace990a91cf80d8d35fc
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2024-05-31 00:33:06 -0400

    Stateless Widgets.

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,30 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       title: 'My Flutter app',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'My Coffee Id',
-            style: TextStyle(color: Colors.white),
-          ),
-          backgroundColor: Colors.brown[700],
-          centerTitle: true,
-        ),
-        body: const Center(
-          child: Text('Hello, ninja'),
-        ),
-      ),
+      home: Home(),
     ),
   );
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'My Coffee Id',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.brown[700],
+        centerTitle: true,
+      ),
+      body: const Center(
+        child: Text('Hello, ninja!'),
+      ),
+    );
+  }
 }
